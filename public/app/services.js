b54fe9ddf88e9769163015dd6d51b8f045df9210
zaturnvalley <zacharycmason@gmail.com>
2016-09-21 15:37:54 -0700

    added AUTH factory

--- public/app/services.js
+++ public/app/services.js
@@ -1,4 +1,32 @@
 angular.module('RecipeServices', ['ngResource'])
 .factory('Recipe', ['$resource', function($resource) {
   return $resource('/api/recipes/:id');
+}])
+.factory('Auth', ['$window', function($window) {
+  return {
+    saveToken: function(token) {
+      $window.localStorage['secretrecipes-token'] = token;
+    },
+    getToken: function() {
+      return $window.localStorage['secretrecipes-token']
+    },
+    removeToken: function() {
+      $window.localStorage.removeItem('secretrecipes-token');
+    },
+    isLoggedIn: function() {
+      var token = this.getToken();
+      return token ? true : false;
+    },
+    currentUser: function() {
+      if(this.isLoggedIn()) {
+        var token = this.getToken();
+        try {
+          var payload = JSON.parse($window.atob(token.split('.')[1]));
+          return payload;
+        } catch (err) {
+          return false;
+        }
+      }
+    }
+  }
 }]);
